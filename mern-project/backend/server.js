30c7afac5afad8b557d42b1dcacbf24988633048
apple <apple@MacBook-Pro.local>
2026-02-11 14:53:23 +0530

    Project upload

---
+++ mern-project/backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// API route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello from backend..." });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
